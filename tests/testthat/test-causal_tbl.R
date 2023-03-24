f586fb4f4e6d29934ad408622b0b545adf512bd3
Christopher Kenny <ctk44@cornell.edu>
2023-03-23 22:57:06 -0400

    Update test-causal_tbl.R

--- tests/testthat/test-causal_tbl.R
+++ tests/testthat/test-causal_tbl.R
@@ -9,7 +9,7 @@ test_that("causal_tbl creation", {
     expect_true(length(names(attr(x, 'causal_cols'))) == 2L)
 })
 
-test_that("causal_tbl attribute", {
+test_that("causal_tbl attributes", {
     x <- causal_tbl(milk_first = c(0, 1, 0, 1, 1, 0, 0, 1),
                    guess = c(0, 1, 0, 1, 1, 0, 0, 1),
                    correct = c(0, 1, 0, 1, 1, 0, 0, 1) == c(0, 1, 0, 1, 1, 0, 0, 1),
@@ -19,6 +19,16 @@ test_that("causal_tbl attribute", {
 
     expect_true(attr(x, 'causal_cols')$outcome == 'correct')
     expect_true(is.null(attr(x, 'causal_cols')$treatment))
+
+    x <- causal_tbl(milk_first = c(0, 1, 0, 1, 1, 0, 0, 1),
+                    guess = c(0, 1, 0, 1, 1, 0, 0, 1),
+                    correct = c(0, 1, 0, 1, 1, 0, 0, 1) == c(0, 1, 0, 1, 1, 0, 0, 1),
+                    .treatment = 'milk_first')
+
+    expect_s3_class(x, c("causal_tbl", "tbl_df", "tbl", "data.frame"), exact=TRUE)
+
+    expect_true(is.null(attr(x, 'causal_cols')$outcome))
+    expect_true(attr(x, 'causal_cols')$treatment == 'milk_first')
 
     x <- causal_tbl(milk_first = c(0, 1, 0, 1, 1, 0, 0, 1),
                     guess = c(0, 1, 0, 1, 1, 0, 0, 1),
