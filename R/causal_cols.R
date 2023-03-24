399745497c442fdb213602db7e021b8a0600f7c9
Cory McCartan <cory.mccartan@gmail.com>
2023-03-24 01:23:55 -0400

    panel, coercion, test errors

--- R/causal_cols.R
+++ R/causal_cols.R
@@ -8,12 +8,13 @@ causal_cols <- function(data) {
 }
 
 
-#' Use `outcome` Attribute for a `causal_tbl`
+#' Define an outcome variable for a `causal_tbl`
 #'
 #' @param data a data frame or `causal_tbl`
-#' @param outcome column name of outcome (tidy-selected)
+#' @param outcome the column containing the outcome variable (tidy-selected).
+#'    Must be numeric or coercible to numeric.
 #'
-#' @return A `causal_tbl`, otherwise the column name of the outcome
+#' @return A `causal_tbl`
 #' @export
 #'
 #' @examples
@@ -28,6 +29,7 @@ set_outcome <- function(data, outcome) {
     data <- as_causal_tbl(data)
     col <- single_col_name(enquo(outcome), data, "outcome")
     causal_cols(data)$outcome <- col
+    data[[col]] <- vctrs::vec_cast(data[[col]], numeric(), x_arg=col)
     data
 }
 
@@ -37,11 +39,20 @@ set_outcome <- function(data, outcome) {
 get_outcome <- function(data) {
     causal_cols(data)$outcome
 }
+#' @rdname set_outcome
+#' @return For `has_outcome()`, `TRUE` if there is an outcome variable set
+#' @export
+has_outcome <- function(data) {
+    !is.null(causal_cols(data)$outcome)
+}
 
-#' Use `treatment` Attribute for a `causal_tbl`
+
+
+#' Define a treatment variable for a `causal_tbl`
 #'
 #' @param data a data frame or `causal_tbl`
-#' @param treatment column name of treatment (tidy-selected)
+#' @param outcome the column containing the treatment variable (tidy-selected).
+#'    Must be numeric or coercible to numeric.
 #'
 #' @return A `causal_tbl`
 #' @export
@@ -58,6 +69,7 @@ set_treatment <- function(data, treatment) {
     data <- as_causal_tbl(data)
     col <- single_col_name(enquo(treatment), data, "treatment")
     causal_cols(data)$treatment <- col
+    data[[col]] <- vctrs::vec_cast(data[[col]], numeric(), x_arg=col)
     data
 }
 
@@ -67,11 +79,64 @@ set_treatment <- function(data, treatment) {
 get_treatment <- function(data) {
     causal_cols(data)$treatment
 }
+#' @rdname set_treatment
+#' @return For `has_treatment()`, `TRUE` if there is a treatment variable set
+#' @export
+has_treatment <- function(data) {
+    !is.null(causal_cols(data)$treatment)
+}
 
 
+
+#' Define a panel data structure for a `causal_tbl`
+#'
+#' @param data a data frame or `causal_tbl`
+#' @param unit the column indexing treatment units (tidy-selected)
+#' @param time the column indexing treatment time (tidy-selected)
+#'
+#' @return A `causal_tbl`
+#' @export
+#'
+#' @examples
+#' data <- data.frame(
+#'   id = c("a", "a", "a", "a", "b", "b", "b", "b"),
+#'   year = rep(2015:2018, 2),
+#'   trt = c(0, 0, 0, 0, 0, 0, 1, 1),
+#'   y = c(1, 3, 2, 3, 1, 3, 4, 5)
+#' ) |>
+#'   set_panel(unit=id, time=year)
+#' print(data) # a causal_tbl
+#' get_panel(data)
+set_panel <- function(data, unit, time) {
+    data <- as_causal_tbl(data)
+    col_unit <- single_col_name(enquo(unit), data, "unit")
+    col_time <- single_col_name(enquo(time), data, "time")
+    causal_cols(data)$panel <- list(unit = col_unit,
+                                    time = col_time)
+    data[[col_time]] <- vctrs::vec_cast(data[[col_time]], integer(), x_arg=col_time)
+    data
+}
+
+#' @rdname set_treatment
+#' @return For `get_panel()` a list with the column names of the unit and time variables
+#' @export
+get_panel <- function(data) {
+    causal_cols(data)$panel
+}
+#' @rdname set_treatment
+#' @return For `has_panel()`, `TRUE` if there is panel data structure
+#' @export
+has_panel <- function(data) {
+    is.list(causal_cols(data)$panel)
+}
+
+
+# Helper
 single_col_name <- function(expr, data, arg) {
-    idx <- tidyselect::eval_select(expr, data, allow_rename=FALSE, allow_empty=FALSE)
-    if (length(idx) > 1) {
+    idx <- tidyselect::eval_select(expr, data, allow_rename=FALSE)
+    if (length(idx) == 0) {
+        cli_abort("Must select a column for {.arg {arg}}", call=parent.frame())
+    } else if (length(idx) > 1) {
         cli_abort("Only one column name is allowed for {.arg {arg}}", call=parent.frame())
     }
     names(data)[idx]
--- tests/testthat/test-causal_cols.R
+++ tests/testthat/test-causal_cols.R
@@ -4,12 +4,18 @@ test_that("getting and setting treatment works", {
 
     x_trt = set_treatment(x, milk_first)
     expect_equal(get_treatment(x_trt), "milk_first")
+    # check non-tidy works too
+    x_trt = set_treatment(x, "milk_first")
+    expect_equal(get_treatment(x_trt), "milk_first")
 
     x = data.frame(milk_first = c(0, 1, 0, 1, 1, 0, 0, 1),
                    guess = c(0, 1, 0, 1, 1, 0, 0, 1))
     x_trt = set_treatment(x, milk_first)
     expect_no_error(validate_causal_tbl(x_trt))
     expect_equal(get_treatment(x_trt), "milk_first")
+
+    expect_error(set_treatment(x, not_a_column), "doesn't exist")
+    expect_error(set_treatment(x, NULL), "Must select")
 })
 
 test_that("getting and setting outcome works", {
@@ -28,4 +34,29 @@ test_that("getting and setting outcome works", {
     x_out = set_outcome(x, guess)
     expect_no_error(validate_causal_tbl(x_out))
     expect_equal(get_outcome(x_out), "guess")
+
+    expect_error(set_outcome(x, not_a_column), "doesn't exist")
+    expect_error(set_outcome(x, NULL), "Must select")
+    x$guess = as.character(x$guess)
+    expect_error(set_outcome(x, "guess"), "convert `guess`")
 })
+
+
+test_that("getting and setting panel data", {
+    x <- data.frame(
+        id = c("a", "a", "a", "a", "b", "b", "b", "b"),
+        year = rep(2015:2018, 2),
+        trt = c(0, 0, 0, 0, 0, 0, 1, 1),
+        y = c(1, 3, 2, 3, 1, 3, 4, 5)
+    )
+
+    x_panel = set_panel(x, unit=id, time=year)
+    expect_equal(get_panel(x_panel), list(unit = "id", time = "year"))
+    expect_no_error(validate_causal_tbl(x_panel))
+
+    expect_error(set_panel(x, unit=id), "for `time`")
+    expect_error(set_panel(x, time=year), "for `unit`")
+    expect_error(set_panel(x, unit=year, time=id), "convert `id`")
+    expect_error(set_panel(x, unit=id, time=not_a_column), "doesn't exist")
+})
+
