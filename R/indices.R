9f410cb60d48216243ad3a007d03f4e639a60998
Christopher Kenny <ctk44@cornell.edu>
2023-03-25 19:46:23 -0400

    rename indices -> causal_idx

--- R/indices.R
+++ R/indices.R
@@ -1,58 +1,58 @@
-#' @describeIn indices Construct an `indices` list with minimal checks
+#' @describeIn causal_idx Construct a `causal_idx` list with minimal checks
 #' @export
-new_indices <- function(x = list()) {
+new_causal_idx <- function(x = list()) {
     if (!is.list(x)) {
         cli_abort("{.arg x} must be a list.")
     }
 
-    vctrs::new_vctr(x, class="indices")
+    vctrs::new_vctr(x, class="causal_idx")
 }
 
-#' Construct a list of indices
+#' Construct a list of indices of type `causal_idx`
 #'
-#' An `indices` list is a list with integer vector entries which represent
+#' A `causal_idx` list is a list with integer vector entries which represent
 #' indices in some other object (like a data frame). Generically it can
 #' be used to represent a graph structure, such as an interference network
 #' or a collection of matched objects.
-#' The main feature of `indices` is that the index references are preserved
+#' The main feature of `causal_idx` is that the index references are preserved
 #' through slicing and reordering. Indices that no longer refer to elements
 #' (because of subsetting) are set to NA.
 #'
 #' @param x
-#' * For `indices()` and `new_indices()`: A list of indices
-#' * For `is_indices()`: An object to test
-#' * For `as_indices()`: An object to coerce
+#' * For `causal_idx()` and `new_causal_idx()`: A list of indices of type `causal_idx`
+#' * For `is_causal_idx()`: An object to test
+#' * For `as_causal_idx()`: An object to coerce
 #'
-#' @returns An `indices` object.
+#' @returns A `causal_idx` object.
 #'
 #' @examples
-#' idx <- indices(list(2, c(1, NA, 3), 2))
+#' idx <- causal_idx(list(2, c(1, NA, 3), 2))
 #' print(idx)
 #' idx[1:2] # subsetting
 #' idx[c(2, 1, 3)] # reordering
 #' @export
-indices <- function(x = list()) {
+causal_idx <- function(x = list()) {
     # convert each element to an integer
-    x <- relist(vctrs::vec_cast(unlist(x), integer(), x_arg="x"), x)
-    new_indices(x)
+    x <- utils::relist(vctrs::vec_cast(unlist(x), integer(), x_arg="x"), x)
+    new_causal_idx(x)
 }
 
-#' @describeIn indices Return `TRUE` if an object is an `indices` list
+#' @describeIn causal_idx Return `TRUE` if an object is an `causal_idx` list
 #' @export
-is_indices <- function(x) {
-    inherits(x, "indices")
+is_causal_idx <- function(x) {
+    inherits(x, "causal_idx")
 }
 
-#' @describeIn indices Coerce an object to an `indices` list
+#' @describeIn causal_idx Coerce an object to an `causal_idx` list
 #' @export
-as_indices <- function(x) {
-    vctrs::vec_cast(x, new_indices())
+as_causal_idx <- function(x) {
+    vctrs::vec_cast(x, new_causal_idx())
 }
 
 
 # printing
 #' @export
-format.indices <- function(x, ...) {
+format.causal_idx <- function(x, ...) {
     vapply(vctrs::vec_data(x), format_index_line, "")
 }
 format_index_line <- function(y) {
@@ -62,7 +62,7 @@ format_index_line <- function(y) {
 # vctrs -------------------------------------------------------------------
 
 #' @export
-`[.indices` <- function(x, i) {
+`[.causal_idx` <- function(x, i) {
     lookup <- match(seq_along(x), i)
     out <- NextMethod()
     for (j in seq_along(out)) {
@@ -72,19 +72,19 @@ format_index_line <- function(y) {
 }
 
 #' @importFrom vctrs vec_ptype_abbr
-#' @method vec_ptype_abbr indices
+#' @method vec_ptype_abbr causal_idx
 #' @export
-vec_ptype_abbr.indices <- function(x, ...) {
+vec_ptype_abbr.causal_idx <- function(x, ...) {
     "idx" # nocov
 }
 
 #' @importFrom vctrs vec_ptype2
 #' @export
-vec_ptype2.list.indices <- function(x, y, ...) list()
+vec_ptype2.list.causal_idx <- function(x, y, ...) list()
 #' @export
-vec_ptype2.indices.list <- function(x, y, ...) list()
+vec_ptype2.causal_idx.list <- function(x, y, ...) list()
 #' @importFrom vctrs vec_cast
 #' @export
-vec_cast.list.indices <- function(x, to, ...) as.list(x)
+vec_cast.list.causal_idx <- function(x, to, ...) as.list(x)
 #' @export
-vec_cast.indices.list <- function(x, to, ...) indices(x)
+vec_cast.causal_idx.list <- function(x, to, ...) causal_idx(x)
---
+++ tests/testthat/test-causal_idx.R
@@ -0,0 +1,36 @@
+test_that("causal_idx constructor", {
+    idx <- causal_idx(list(2, c(1, NA, 3), 2))
+    expect_s3_class(idx, "causal_idx")
+    expect_type(idx, "list")
+    expect_true(is_causal_idx(idx))
+
+    expect_error(causal_idx(list("a")), "character")
+    expect_error(causal_idx(5), "must be a list")
+})
+
+test_that("causal_idx conversion", {
+    idx <- causal_idx(list(2, c(1, NA, 3), 2))
+
+    expect_s3_class(as_causal_idx(as.list(idx)), "causal_idx")
+    expect_type(as.list(idx), "list")
+    expect_type(c(idx, list()), "list")
+    expect_type(c(list(), idx), "list")
+})
+
+test_that("causal_idx slicing", {
+    idx <- causal_idx(list(2, c(1, NA, 3), 2))
+
+    expect_equal(idx[1:3], idx)
+    expect_equal(idx[1:2],
+                 causal_idx(list(2, c(1, NA, NA))))
+    expect_equal(idx[2:1],
+                 causal_idx(list(c(2, NA, NA), 1)))
+})
+
+
+test_that("causal_idx printing", {
+    idx <- causal_idx(list(2, c(1, NA, 3), 2))
+
+    expect_snapshot(print(idx))
+    expect_snapshot(str(idx))
+})
